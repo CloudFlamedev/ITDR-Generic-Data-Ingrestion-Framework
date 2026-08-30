2555a5c452de35d75bfd6c0b77f12bf4cb6da7a9
CloudFlamedev <utkristg@gmail.com>
2026-08-30 20:10:00 +0530

    Fix formatting and add comments for clarity in ITDRRecord model

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Column, Integer, String, JSON
 
-from app.database import Base
+from app.database import Base 
 
 
 class ITDRRecord(Base):
 
-    __tablename__ = "itdr_records"
+    __tablename__ = "itdr_records" # this is the name of the table in the database
 
-    id = Column(Integer, primary_key=True, index=True)
-    event_id = Column(String, nullable=False, unique=True, index=True)
-    source = Column(String, nullable=False)
-    raw_data = Column(JSON, nullable=False)
+    id = Column(Integer, primary_key=True, index=True) # Unique ID for each database row.
+    event_id = Column(String, nullable=False, unique=True, index=True) ## Unique identifier for each event. Used to identify duplicate events.
+    source = Column(String, nullable=False) #This stores where the event came from. like- Test data, AWS, AZURE etc(CSV, JSON, XML)
+    raw_data = Column(JSON, nullable=False) # Stores the complete original event data as JSON.
